d90e96ca38c28f0c2b50a814b866c2f5a1fce636
yjgaia <yjdev88@gmail.com>
2024-11-17 16:37:20 +0900

    Refactor WalletLoginManager methods to use camelCase naming; add getBalance, readContract, and writeContract methods for enhanced functionality

--- lib/WalletLoginManager.d.ts
+++ lib/WalletLoginManager.d.ts
@@ -1,15 +1,20 @@
 import { AuthTokenManager } from "@common-module/supabase";
+import { Config, ReadContractParameters, WriteContractParameters } from "@wagmi/core";
+import type { Abi, ContractFunctionArgs, ContractFunctionName } from "viem";
 declare class WalletLoginManager extends AuthTokenManager<{
     loginStatusChanged: (loggedIn: boolean) => void;
 }> {
-    get loggedInWallet(): string | undefined;
-    get loggedInAddress(): string | undefined;
-    get loggedInUser(): string | undefined;
-    get isLoggedIn(): boolean;
+    getLoggedInWallet(): string | undefined;
+    getLoggedInAddress(): `0x${string}` | undefined;
+    getLoggedInUser(): `0x${string}` | undefined;
+    isLoggedIn(): boolean;
     constructor();
     init(): void;
     login(): Promise<string>;
     logout(): void;
+    getBalance(chainId: number): Promise<void>;
+    readContract<const abi extends Abi | readonly unknown[], functionName extends ContractFunctionName<abi, "pure" | "view">, args extends ContractFunctionArgs<abi, "pure" | "view", functionName>>(parameters: ReadContractParameters<abi, functionName, args, Config>): Promise<import("viem").ContractFunctionReturnType<abi, "pure" | "view", functionName, args>>;
+    writeContract<const abi extends Abi | readonly unknown[], functionName extends ContractFunctionName<abi, "nonpayable" | "payable">, args extends ContractFunctionArgs<abi, "nonpayable" | "payable", functionName>, chainId extends Config["chains"][number]["id"]>(parameters: WriteContractParameters<abi, functionName, args, Config, chainId>): Promise<`0x${string}`>;
 }
 declare const _default: WalletLoginManager;
 export default _default;
--- src/WalletLoginManager.ts
+++ src/WalletLoginManager.ts
@@ -1,21 +1,28 @@
 import { AuthTokenManager } from "@common-module/supabase";
 import { WalletSessionManager } from "@common-module/wallet";
+import {
+  Config,
+  ReadContractParameters,
+  WriteContractParameters,
+} from "@wagmi/core";
+import type { Abi, ContractFunctionArgs, ContractFunctionName } from "viem";
 import WalletLoginModal from "./components/WalletLoginModal.js";
 
 class WalletLoginManager extends AuthTokenManager<{
   loginStatusChanged: (loggedIn: boolean) => void;
 }> {
-  public get loggedInWallet() {
+  public getLoggedInWallet() {
     return this.store.get<string>("loggedInWallet");
   }
-  public get loggedInAddress() {
-    return this.store.get<string>("loggedInAddress");
+  public getLoggedInAddress() {
+    return this.store.get<`0x${string}`>("loggedInAddress");
   }
-  public get loggedInUser() {
-    return this.loggedInAddress;
+  public getLoggedInUser() {
+    return this.getLoggedInAddress();
   }
-  public get isLoggedIn() {
-    return !!this.token && !!this.loggedInWallet && !!this.loggedInAddress;
+  public isLoggedIn() {
+    return !!this.token && !!this.getLoggedInWallet() &&
+      !!this.getLoggedInAddress();
   }
 
   constructor() {
@@ -34,7 +41,7 @@ class WalletLoginManager extends AuthTokenManager<{
     this.store.setPermanent("loggedInWallet", walletId);
     this.store.setPermanent("loggedInAddress", walletAddress);
 
-    this.emit("loginStatusChanged", this.isLoggedIn);
+    this.emit("loginStatusChanged", this.isLoggedIn());
 
     return walletAddress;
   }
@@ -46,7 +53,48 @@ class WalletLoginManager extends AuthTokenManager<{
     this.store.remove("loggedInWallet");
     this.store.remove("loggedInAddress");
 
-    this.emit("loginStatusChanged", this.isLoggedIn);
+    this.emit("loginStatusChanged", this.isLoggedIn());
+  }
+
+  public async getBalance(chainId: number) {
+    const walletAddress = this.getLoggedInAddress();
+    if (!walletAddress) throw new Error("Not logged in");
+    await WalletSessionManager.getBalance(chainId, walletAddress);
+  }
+
+  public async readContract<
+    const abi extends Abi | readonly unknown[],
+    functionName extends ContractFunctionName<abi, "pure" | "view">,
+    args extends ContractFunctionArgs<abi, "pure" | "view", functionName>,
+  >(parameters: ReadContractParameters<abi, functionName, args, Config>) {
+    return await WalletSessionManager.readContract(parameters);
+  }
+
+  public async writeContract<
+    const abi extends Abi | readonly unknown[],
+    functionName extends ContractFunctionName<abi, "nonpayable" | "payable">,
+    args extends ContractFunctionArgs<
+      abi,
+      "nonpayable" | "payable",
+      functionName
+    >,
+    chainId extends Config["chains"][number]["id"],
+  >(
+    parameters: WriteContractParameters<
+      abi,
+      functionName,
+      args,
+      Config,
+      chainId
+    >,
+  ) {
+    if (!this.getLoggedInAddress()) throw new Error("Not connected");
+    if (
+      WalletSessionManager.getConnectedAddress() !== this.getLoggedInAddress()
+    ) {
+      throw new Error("Wallet address mismatch");
+    }
+    return await WalletSessionManager.writeContract(parameters as any);
   }
 }
 
