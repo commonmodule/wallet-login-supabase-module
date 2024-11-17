0de136d11d961f771d4608bf624c496cfeba0083
yjgaia <yjdev88@gmail.com>
2024-11-17 10:42:09 +0900

    Remove WalletLoginConfig and WalletLoginManager classes; update package version to 0.2.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,1 @@
-export { default as WalletLoginConfig } from "./WalletLoginConfig.js";
-export { default as WalletLoginManager } from "./WalletLoginManager.js";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as WalletLoginConfig } from "./WalletLoginConfig.js";
-export { default as WalletLoginManager } from "./WalletLoginManager.js";
